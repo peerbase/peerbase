e5ac7bb2868fd9668079d7d6ff7259f5dea7e6c6
tav <tav@espians.com>
2018-09-08 02:50:25 +0100

    process: add support for managing the system process

---
+++ process/process.go
@@ -0,0 +1,176 @@
+// Public Domain (-) 2010-present, The Peerbase Authors.
+// See the Peerbase UNLICENSE file for details.
+
+// Package process provides utilities to manage the current system process.
+package process
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"sync"
+	"syscall"
+)
+
+var (
+	exitDisabled bool
+	exiting      bool
+	mu           sync.RWMutex // protects exitDisabled, exiting, registry
+)
+
+var (
+	osExit   = os.Exit
+	registry = make(map[os.Signal][]func())
+	wait     = make(chan struct{})
+)
+
+type lockFile struct {
+	file string
+	link string
+}
+
+func (l *lockFile) release() {
+	os.Remove(l.file)
+	os.Remove(l.link)
+}
+
+// CreatePIDFile writes the current process ID to a new file at the given path.
+// The written file is removed when the process exits on receiving an
+// os.Interrupt or SIGTERM signal — either directly or through the Exit call.
+func CreatePIDFile(path string) error {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0660)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(f, "%d", os.Getpid())
+	err = f.Close()
+	if err == nil {
+		SetExitHandler(func() {
+			os.Remove(path)
+		})
+	}
+	return err
+}
+
+// DisableDefaultExit will prevent the process from automatically exiting after
+// processing os.Interrupt or SIGTERM signals. The disabled status will not be
+// enforced if Exit is called.
+func DisableDefaultExit() {
+	mu.Lock()
+	exitDisabled = true
+	mu.Unlock()
+}
+
+// Exit runs the registered exit handlers, as if the os.Interrupt signal had
+// been sent, and then terminates the process with the given status code. Exit
+// blocks until the process terminates if it has already been called elsewhere.
+func Exit(code int) {
+	mu.Lock()
+	if exiting {
+		mu.Unlock()
+		<-wait
+		return
+	}
+	exiting = true
+	handlers := registry[os.Interrupt]
+	mu.Unlock()
+	for _, handler := range handlers {
+		handler()
+	}
+	osExit(code)
+}
+
+// Init acquires a process lock and writes the PID file for the current process.
+// It returns false for the locked status if there was an error acquiring the
+// process lock.
+func Init(directory string, name string) (locked bool, err error) {
+	err = Lock(directory, name)
+	if err != nil {
+		return false, err
+	}
+	return true, CreatePIDFile(filepath.Join(directory, name+".pid"))
+}
+
+// Lock tries to acquire a process lock in the given directory. The acquired
+// lock file is released when the process exits on receiving an os.Interrupt or
+// SIGTERM signal — either directly or through the Exit call.
+//
+// This function has only been tested for correctness on Unix systems with
+// filesystems where link is atomic. It may not work as expected on NFS mounts
+// or on platforms like Windows.
+func Lock(directory string, name string) error {
+	file := filepath.Join(directory, fmt.Sprintf("%s-%d.lock", name, os.Getpid()))
+	f, err := os.OpenFile(file, os.O_CREATE|os.O_WRONLY, 0660)
+	if err != nil {
+		return err
+	}
+	f.Close()
+	link := filepath.Join(directory, name+".lock")
+	err = os.Link(file, link)
+	if err != nil {
+		// We don't remove the lock file here so that calling Lock multiple
+		// times from the same process doesn't remove an existing lock.
+		return err
+	}
+	l := &lockFile{
+		file: file,
+		link: link,
+	}
+	SetExitHandler(l.release)
+	return nil
+}
+
+// ResetHandlers drops all currently registered handlers.
+func ResetHandlers() {
+	mu.Lock()
+	registry = map[os.Signal][]func(){}
+	mu.Unlock()
+}
+
+// SetExitHandler registers the given handler function to run when receiving
+// os.Interrupt or SIGTERM signals.
+func SetExitHandler(handler func()) {
+	mu.Lock()
+	registry[os.Interrupt] = prepend(registry[os.Interrupt], handler)
+	registry[syscall.SIGTERM] = prepend(registry[syscall.SIGTERM], handler)
+	mu.Unlock()
+}
+
+// SetSignalHandler registers the given handler function to run when receiving
+// the specified signal.
+func SetSignalHandler(signal os.Signal, handler func()) {
+	mu.Lock()
+	registry[signal] = prepend(registry[signal], handler)
+	mu.Unlock()
+}
+
+func handleSignals() {
+	notifier := make(chan os.Signal, 100)
+	signal.Notify(notifier)
+	for sig := range notifier {
+		mu.RLock()
+		handlers, found := registry[sig]
+		mu.RUnlock()
+		if found {
+			for _, handler := range handlers {
+				handler()
+			}
+		}
+		mu.RLock()
+		if !exitDisabled {
+			if sig == syscall.SIGTERM || sig == os.Interrupt {
+				osExit(1)
+			}
+		}
+		mu.RUnlock()
+	}
+}
+
+func prepend(xs []func(), handler func()) []func() {
+	return append([]func(){handler}, xs...)
+}
+
+func init() {
+	go handleSignals()
+}
---
+++ process/process_test.go
@@ -0,0 +1,209 @@
+// Public Domain (-) 2018-present, The Peerbase Authors.
+// See the Peerbase UNLICENSE file for details.
+
+package process
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"syscall"
+	"testing"
+	"time"
+
+	"peerbase.net/go/mock/osexit"
+)
+
+func TestCreatePIDFile(t *testing.T) {
+	tmp := setup(t)
+	defer cleanup(tmp)
+	fpath := filepath.Join(tmp, "test.pid")
+	err := CreatePIDFile(fpath)
+	if err != nil {
+		t.Fatalf("Unexpected error creating PID file: %s", err)
+	}
+	written, err := ioutil.ReadFile(fpath)
+	if err != nil {
+		t.Fatalf("Unexpected error reading PID file: %s", err)
+	}
+	expected := os.Getpid()
+	pid, err := strconv.ParseInt(string(written), 10, 64)
+	if err != nil {
+		t.Fatalf("Unexpected error parsing PID file contents as an int: %s", err)
+	}
+	if expected != int(pid) {
+		t.Fatalf("Mismatching PID file contents: expected %d, got %d", expected, int(pid))
+	}
+	osExit = osexit.Set()
+	Exit(2)
+	if !osexit.Called() || osexit.Status() != 2 {
+		t.Fatalf("Exit call did not behave as expected")
+	}
+	_, err = os.Stat(fpath)
+	if err == nil {
+		t.Fatalf("Calling Exit did not remove the created PID file as expected")
+	}
+	if !os.IsNotExist(err) {
+		t.Fatalf("Calling Exit did not remove the created PID file as expected, got error: %s", err)
+	}
+	fpath = filepath.Join(tmp+"-nonexistent-directory", "test.pid")
+	err = CreatePIDFile(fpath)
+	if err == nil {
+		t.Fatalf("Expected an error when creating PID file in a non-existent directoy")
+	}
+}
+
+func TestExit(t *testing.T) {
+	ResetHandlers()
+	called := false
+	SetExitHandler(func() {
+		called = true
+	})
+	osExit = osexit.Set()
+	resetExit()
+	Exit(7)
+	if !osexit.Called() {
+		t.Fatalf("Exit did not call os.Exit")
+	}
+	status := osexit.Status()
+	if status != 7 {
+		t.Fatalf("Exit did not set the right status code: expected 7; got %d", status)
+	}
+	if !called {
+		t.Fatalf("Exit handler was not called on calling Exit")
+	}
+	osexit.Reset()
+	go func() {
+		Exit(8)
+	}()
+	select {
+	case wait <- struct{}{}:
+	case <-time.After(time.Second):
+		t.Fatalf("Second call to Exit did not hang on wait within 1s")
+	}
+	if osexit.Called() {
+		t.Fatalf("Second call to Exit called os.Exit")
+	}
+}
+
+func TestDisableDefaultExit(t *testing.T) {
+	ResetHandlers()
+	called := false
+	SetExitHandler(func() {
+		called = true
+	})
+	osExit = osexit.Set()
+	resetExit()
+	send(syscall.SIGTERM)
+	if !osexit.Called() {
+		t.Fatalf("os.Exit was not called on SIGTERM")
+	}
+	if !called {
+		t.Fatalf("Exit handler not called on SIGTERM")
+	}
+	DisableDefaultExit()
+	called = false
+	osexit.Reset()
+	resetExit()
+	send(syscall.SIGTERM)
+	if osexit.Called() {
+		t.Fatalf("os.Exit was called on SIGTERM even when DisableDefaultExit had been called")
+	}
+	if !called {
+		t.Fatalf("Exit handler not called on the second SIGTERM")
+	}
+}
+
+func TestInit(t *testing.T) {
+	tmp := setup(t)
+	defer cleanup(tmp)
+	locked, err := Init(tmp, "peerbase")
+	if err != nil {
+		t.Fatalf("Unexpected error initialising process: %s", err)
+	}
+	if !locked {
+		t.Fatalf("Init failed to acquire process lock")
+	}
+	locked, err = Init(tmp+"-nonexistent-directory", "peerbase")
+	if locked {
+		t.Fatalf("Successfully acquired a process lock in a non-existing directory")
+	}
+	if err == nil {
+		t.Fatalf("Expected an error when calling Init in a non-existing directory")
+	}
+}
+func TestLock(t *testing.T) {
+	tmp := setup(t)
+	defer cleanup(tmp)
+	err := Lock(tmp, "peerbase")
+	if err != nil {
+		t.Fatalf("Unexpected error acquiring Lock: %s", err)
+	}
+	err = Lock(tmp, "peerbase")
+	if err == nil {
+		t.Fatalf("Expected an error when calling Lock on an already locked path")
+	}
+	fpath := filepath.Join(tmp, fmt.Sprintf("peerbase-%d.lock", os.Getpid()))
+	_, err = os.Stat(fpath)
+	if err != nil {
+		t.Fatalf("Unexpected error accessing the raw lock file: %s", err)
+	}
+	osExit = osexit.Set()
+	Exit(2)
+	_, err = os.Stat(fpath)
+	if err == nil {
+		t.Fatalf("Calling Exit did not remove the lock file as expected")
+	}
+	if !os.IsNotExist(err) {
+		t.Fatalf("Calling Exit did not remove the lock file as expected, got error: %s", err)
+	}
+	err = Lock(tmp+"-nonexistent-directory", "peerbase")
+	if err == nil {
+		t.Fatalf("Expected an error when calling Lock in a non-existing directory")
+	}
+}
+
+func TestSignalHandler(t *testing.T) {
+	ResetHandlers()
+	called := false
+	SetSignalHandler(syscall.SIGHUP, func() {
+		called = true
+	})
+	send(syscall.SIGABRT)
+	if called {
+		t.Fatalf("Signal handler erroneously called on SIGABRT")
+	}
+	send(syscall.SIGHUP)
+	if !called {
+		t.Fatalf("Signal handler not called on SIGHUP")
+	}
+}
+
+func cleanup(tmp string) {
+	os.RemoveAll(tmp)
+}
+
+func resetExit() {
+	mu.Lock()
+	exiting = false
+	mu.Unlock()
+}
+
+func send(sig syscall.Signal) {
+	syscall.Kill(syscall.Getpid(), sig)
+	// TODO(tav): Figure out a way of waiting for the signal to be delivered in
+	// a less brittle.
+	time.Sleep(time.Second)
+}
+
+func setup(t *testing.T) string {
+	resetExit()
+	ResetHandlers()
+	tmp, err := ioutil.TempDir("", "peerbase-process")
+	if err != nil {
+		t.Skipf("Unable to create temporary directory for tests: %s", err)
+	}
+	return tmp
+}
