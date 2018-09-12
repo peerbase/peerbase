afcc5b47714766da7ff597abf6fe973c8fa20a0f
tav <tav@espians.com>
2018-09-12 20:32:39 +0100

    cmd: add stub kairos and peerbase commands

---
+++ cmd/kairos/kairos.go
@@ -0,0 +1,10 @@
+// Command kairos is a tool for experimenting with Kairos consensus.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, kairos")
+}
---
+++ cmd/peerbase/peerbase.go
@@ -0,0 +1,10 @@
+// Command peerbase is a tool for running Peerbase.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, peerbase")
+}
