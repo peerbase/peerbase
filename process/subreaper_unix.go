12bb3f27c57850decc37bec697f1f25aa7c41ad2
tav <tav@espians.com>
2018-09-13 00:50:07 +0100

    process: fix build constraint

--- process/subreaper_unix.go
+++ process/subreaper_unix.go
@@ -1,7 +1,7 @@
 // Public Domain (-) 2018-present, The Peerbase Authors.
 // See the Peerbase UNLICENSE file for details.
 
-// +build !linux !windows
+// +build !linux,!windows
 
 package process
 
